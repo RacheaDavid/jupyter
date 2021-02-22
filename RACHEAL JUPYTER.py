f28d82aed02a4931189e88dc8922b5efd8b0ff2c
RacheaDavid <79477298+RacheaDavid@users.noreply.github.com>
2021-02-22 23:46:35 +0400

    JUPYTER NOTEBOOK

---
+++ RACHEAL JUPYTER.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("HELLO WORLD")
+
+
+# In[2]:
+
+
+A=23
+B=32
+print(A+B)
+
+
+# In[3]:
+
+
+for C in range(1,5):
+    print("SMILE")
+
+
+# In[4]:
+
+
+D=("I AM DONE")
+E=(" Yeahhhh hoho")
+print(D+E)
+
+
+# In[ ]:
+
+
+
+
